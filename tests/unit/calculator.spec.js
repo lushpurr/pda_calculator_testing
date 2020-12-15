9d344007065fc636dbda16ac6bf3b1e0c7daffdd
lushpurr <lushpurr@gmail.com>
2020-12-15 22:43:34 +0000

    add test completed

---
+++ tests/unit/calculator.spec.js
@@ -0,0 +1,19 @@
+import { expect } from 'chai'
+import { shallowMount } from '@vue/test-utils'
+import App from '@/App.vue'
+
+describe('App.vue', () => {
+  it('enterNum changes running total', () => {
+    const wrapper = shallowMount(App)
+    wrapper.vm.previousTotal = 4
+    wrapper.vm.add('5');
+    expect(wrapper.vm.runningTotal).to.equal(9)
+  })
+
+  it('should be able to add two numbers', () => {
+    const wrapper = shallowMount(App)
+    wrapper.vm.previousTotal = 4
+    wrapper.vm.add('1');
+    expect (wrapper.vm.runningTotal).to.equal(5)
+  })
+})
