ae1197e4d5109d4e580950f8f432648f89ab5c0a
lushpurr <lushpurr@gmail.com>
2020-12-16 17:21:47 +0000

    error answer is infinity if divide by zero

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -64,7 +64,6 @@ it('should be able to work with negative, positive and very large numbers', () =
 // What does the code do in exceptional circumstances? Specifically, if you divide by zero, what is the effect?
 
 //If you divide by zero the answer is infinity
-//CypressError: Timed out retrying: Expected to find element: 'display', but never found it.
 
 // Write a test to describe what you'd prefer to happen, 
 // and then correct the code to make that test pass 
@@ -75,7 +74,7 @@ it('should be able to divide by zero', () => {
   cy.get('#operator_divide').click();
   cy.get('#number0').click();
   cy.get('#operator_equals').click();
-  cy.get('display').should('contain', '2')
+  cy.get('.display').should('contain', '2')
 })
 
 
