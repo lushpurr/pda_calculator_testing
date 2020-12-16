cf5509fe4e73a1599597a2a140def3b529440ed9
lushpurr <lushpurr@gmail.com>
2020-12-16 17:15:39 +0000

    error when dividing by zero

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -60,5 +60,24 @@ it('should be able to work with negative, positive and very large numbers', () =
   cy.get('.display').should('contain', '-8000000')
 })
 
-// What does the code do in exceptional circumstances? Specifically, if you divide by zero, what is the effect? Write a test to describe what you'd prefer to happen, and then correct the code to make that test pass (you will need to modify the Calculator model to meet this requirement).
+//  
+// What does the code do in exceptional circumstances? Specifically, if you divide by zero, what is the effect?
+
+//If you divide by zero the answer is infinity
+//CypressError: Timed out retrying: Expected to find element: 'display', but never found it.
+
+// Write a test to describe what you'd prefer to happen, 
+// and then correct the code to make that test pass 
+//(you will need to modify the Calculator model to meet this requirement).
+
+it('should be able to divide by zero', () => {
+  cy.get('#number2').click();
+  cy.get('#operator_divide').click();
+  cy.get('#number0').click();
+  cy.get('#operator_equals').click();
+  cy.get('display').should('contain', '2')
+})
+
+
+
 })
